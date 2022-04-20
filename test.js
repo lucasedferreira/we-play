732da7104c074900e8089e1467aac3b477a8410c
Lucas Ferreira <lucasedbatista@gmail.com>
2022-04-19 22:15:23 -0300

    heroku ruim

--- test.js
+++ test.js
@@ -2,7 +2,9 @@ require("dotenv").config();
 
 (async () => {
   console.log("entrou");
+
   const AlertService = require("./domains/Alert/Checker");
   await new AlertService().sendYesterdayVideos();
+
   console.log("saiu");
 })();
